922bcaa1115c70ecec5b4983f35f58ae7c5b6aa2
Iwaki <iwakih2@nialng-02178851.fios-router.home>
2022-02-05 17:39:52 -0500

    t

--- app.py
+++ app.py
@@ -49,18 +49,19 @@ def analyze(b):
     b.sim_invest(return_year=return_year)
     return(b)
 
+def analyze(b):
+    b.sim_loan(down_payment_ratio=down_payment_ratio, years=years, 
+                interest_rate=interest_rate)
+    b.sim_equity(appreciation_year=appreciation_year)
+    b.sim_ex(hoa=hoa, tax_rate=tax_rate, insurance_rate=insurance_rate, 
+                maintenance_rate=maintenance_rate, inflation_year=inflation_year)
+    b.sim_rent(extra_rehab=extra_rehab, rent=rent, vacancy_rate=vacancy_rate, 
+                op_rate=op_rate)
+    b.sim_invest(return_year=return_year)
+    return(b)
+
 def main():
 
-	def analyze(b):
-		b.sim_loan(down_payment_ratio=down_payment_ratio, years=years, 
-					interest_rate=interest_rate)
-		b.sim_equity(appreciation_year=appreciation_year)
-		b.sim_ex(hoa=hoa, tax_rate=tax_rate, insurance_rate=insurance_rate, 
-					maintenance_rate=maintenance_rate, inflation_year=inflation_year)
-		b.sim_rent(extra_rehab=extra_rehab, rent=rent, vacancy_rate=vacancy_rate, 
-					op_rate=op_rate)
-		b.sim_invest(return_year=return_year)
-		return(b)
     
     price = 1000 * st.sidebar.number_input('Property Price (in K)', value=400, step=1)
     rehab_cost = st.sidebar.slider("Rehab Cost", 0, 80000, step=1000, value=10000)
@@ -100,7 +101,7 @@ def main():
     st.text(f'Total initial payment: {b.initial_total:.0f}')
     
     t = get_table(b)
-    d = t.loc[[0, 12, 24, 36, 48, 60, 96, 120, 180, 240, 300,,:]
+    d = t.loc[[0, 12, 24, 36, 48, 60, 96, 120, 180, 240,,:]
     st.line_chart(d[['tg','invest_change']])
     st.table(d[['period', 'end_balance', 'property_value', 'equity', 'invest']])
     
